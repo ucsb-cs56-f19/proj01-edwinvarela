b0e0d77100a6a19d1bce6a7de9a8234ffd97ed83
Edwin Varela <edwinvarela@ucsb.edu>
2019-11-20 18:28:51 -0800

    ev - We are converting JSON objects to Java Objects

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -13,7 +13,21 @@ public class FeatureCollection {
     private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
     public String type;
 
+    public static FeatureCollection fromJSON(String json) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+            FeatureCollection featureCollection = objectMapper.readValue(json, FeatureCollection.class);
+            return featureCollection;
+        } catch (JsonProcessingException jpe) {
+            logger.error("JsonProcessingException:" + jpe);
+            return null;
+        } catch (Exception e) {
+            logger.error("Exception:" + e);
+            return null;
+        }
+    }
    
 }
 

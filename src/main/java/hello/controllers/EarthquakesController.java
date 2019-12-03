93bbc3352c94138547599dd4bb67cbca7c41a559
edwinvarela <edwinvarela@ucsb.edu>
2019-12-02 22:15:24 -0800

    ev - created a seatch directory for Eqsearch and adjusted imports and packages

--- src/main/java/hello/controllers/EarthquakesController.java
+++ src/main/java/hello/controllers/EarthquakesController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import java.util.HashMap;
 
 import hello.services.EarthquakeQueryService;
-import hello.EqSearch;
+import hello.searches.EqSearch;
 import hello.geojson.FeatureCollection;
 
 

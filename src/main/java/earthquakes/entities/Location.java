bde20ac05030a1cc5d1824950542fb25fb79706a
edwinvarela <edwinvarela@ucsb.edu>
2019-12-05 14:27:34 -0800

    ev - adjusted form for add to fav freature

--- src/main/java/earthquakes/entities/Location.java
+++ src/main/java/earthquakes/entities/Location.java
@@ -19,8 +19,8 @@ public class Location {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getplaceId() { return placeId; }
-    public void setplaceId(String x) { this.placeId = x; }
+    public String getPlaceId() { return placeId; }
+    public void setPlaceId(String x) { this.placeId = x; }
    
     public String getName() { return name; }
     public void setName (String x) { this.name = x; }
